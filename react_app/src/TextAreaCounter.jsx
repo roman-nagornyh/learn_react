95fc82df10d67313ec8a4bf59d3110366a498459
nagornyhri <nagornyxr@mail.ru>
2024-01-28 20:42:24 +0700

    last version my branch

--- react_app/src/TextAreaCounter.jsx
+++ react_app/src/TextAreaCounter.jsx
@@ -4,60 +4,43 @@ class TextAreaCounter extends Component {
 
     constructor() {
         super();
-        this.state = {
-            text: '',
-            countCallInsert: 0,
-            countCallDelete: 0,
-            countCallUpdate: 0,
-            countCallFuture: 0,
-        };
+        this.state = {};
+        this.onTextChange = this.onTextChange.bind(this);
 
     }
 
     onTextChange(event) {
-        this.setState({...this.state, text: event.target.value});
+        this.setState({text: event.target.value});
     }
 
     render() {
-        const text = 'text' in this.state ? this.state.text : this.props.text;
         return (
-            <div>
-                <textarea value={text} onChange={event => this.onTextChange(event)}/>
-                <h3>{text.length}</h3>
-            </div>
+            <textarea value={this.state.text} onChange={event => this.onTextChange(event)}/>
         );
     }
 
     componentDidMount() {
-        // Выполняется после первоначальной вставки компонента
-        // В данном методе можно выполнить какую то подгрузуку данных
-        console.log('Я выполняюсь после первоначальной вставки компонента');
-        this.setState({...this.state, countCallInsert: this.state.countCallInsert + 1});
-
+        console.log('componentDidMount');
     }
 
     componentWillUnmount() {
-        // В данном методе может выполняться любая отчистка объекта
-        console.log('Выполнение перед удалением объекта');
-        this.setState({...this.state, countCallDelete: this.state.countCallDelete + 1})
-
+        alert('Я работаю');
+        console.log('componentWillUnmount');
     }
 
     componentDidUpdate(prevProps, prevState, snapshot) {
-        // Данный метод уже имеет новое значение состояния. Его можно использовать
-        // сравнения текущего значения компонетна со старым
-        console.log('Я работаю при обновлении компонента');
-        this.setState({...this.state, countCallUpdate: this.state.countCallUpdate + 1});
-
+        console.log('componentDidUpdate ', prevProps, prevState,
+            snapshot);
     }
 
-    shouldComponentUpdate(nextProps, nextState, nextContext) {
-        // В данном методе можно реализовать логику обновления компонента
-        // и решить, обновлять его или нет
-        // и тем самым провести оптимизацию реакт приложения
-        console.log('В данном методе нам дается будущее состояние компонента');
-        this.setState({...this.state, countCallFuture: this.state.countCallFuture + 1})
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        console.log('getSnapshotBeforeUpdate', prevProps, prevState);
+        return 'hello';
+    }
 
+    shouldComponentUpdate(newProps, newState) {
+        console.log('shouldComponentUpdate ', newProps, newState);
+        return true;
     }
 
 
--- react_app/src/main.jsx
+++ react_app/src/main.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import App from "./App.jsx";
+import TextAreaCounter from "./TextAreaCounter.jsx";
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App></App>
+    <TextAreaCounter></TextAreaCounter>
   </React.StrictMode>,
 )
